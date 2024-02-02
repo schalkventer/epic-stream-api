88bfdd81a391e411d2e0a639194e36b36383cc70
Schalk Venter <venterschalk@gmail.com>
2024-02-02 11:09:27 +0200

    Prevent issues where episodes started a 0 number

---
+++ scripts/prevent-episode-zero.mjs
@@ -0,0 +1,46 @@
+import {
+  readFileSync,
+  writeFileSync,
+  readdirSync,
+} from "fs";
+
+const dir = readdirSync("api/shows");
+
+for (const filename of dir) {
+  const file = JSON.parse(
+    readFileSync(`api/shows/${filename}`, "utf8")
+  );
+
+  const { seasons } = file;
+
+  const newSeasons = seasons.map((season) => {
+    let isMatch = false;
+
+    const { episodes } = season;
+
+    const newEpisodes = episodes.map((inner) => {
+      if (inner.episode === 0) {
+        isMatch = true;
+      }
+
+      if (!isMatch) {
+        return inner;
+      }
+
+      return {
+        ...inner,
+        episode: inner.episode + 1,
+      };
+    });
+
+    return { ...season, episodes: newEpisodes };
+  });
+
+  const string = JSON.stringify(
+    { ...file, seasons: newSeasons },
+    null,
+    2
+  );
+
+  writeFileSync(`api/shows/${filename}`, string, "utf8");
+}
